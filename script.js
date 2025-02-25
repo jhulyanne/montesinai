9355867e591d40d323cb31df9fd06e2c42688c7f
jhulyanne <jhulyannedeoliveira@gmail.com>
2025-02-25 09:34:30 -0300

    :sparkles: feat: block context menu on some images

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function changeImage(imageSrc) {
     document.getElementById("mainImage").src = imageSrc;
 }
+
+// impede o menu de clique direito/pressionar nas imagens que tem a classe "no-right-click"
+document.addEventListener("contextmenu", function (event) {
+    if (event.target.classList.contains("no-right-click")) {
+      event.preventDefault();
+    }
+});
